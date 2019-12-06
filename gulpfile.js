f9121a6619eb33da8da98bdb0b925d1cbe4ecd0f
jbGabantu <gabantuj@gmail.com>
2019-12-06 09:37:45 -0600

    Transpile the JS from ES6 to ES5 using gulp-babel

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const {task, src, dest, parallel, watch} = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
 
 //Autoprefix the CSS using gulp-autoprefixer
 task('autoprefixer', () => {
@@ -27,8 +28,12 @@ task('imagemin', () => {
 });
 
 //Transpile the JS from ES6 to ES5 using gulp-babel
-//Concatenate the JS into a single file (call it main.js) using gulp-concat. These files will need to be concatenated in a specific order so that they are loaded without errors.
-//Minify the JS using gulp-uglify
-//Copy over the HTML.
-//Setup a Gulp watch task that will watch for changes to your CSS, JS, and HTML files and run the necessary Gulp tasks that will generate new files when they change.
-//Setup a gulp task called 'all' to run ALL the gulp tasks.
+task('default', () => {
+  return src('src/js/*')
+  .pipe(babel({
+    presets: ['@babel/env']
+    }))
+  .pipe(dest('dist/js'))
+});
+
+  
