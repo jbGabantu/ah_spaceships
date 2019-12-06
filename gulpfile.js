20de64ebe2090440e9cc785f2555302f7609446e
jbGabantu <gabantuj@gmail.com>
2019-12-06 09:16:31 -0600

    Autoprefix the CSS using gulp-autoprefixer

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const {task, src, dest, parallel, watch} = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+
+//Autoprefix the CSS using gulp-autoprefixer
+task('autoprefixer', () => {
+  return src('src/css/style.css')
+  .pipe(autoprefixer({
+    cascade: false
+  }))
+    .pipe(dest('dist/css'));
+});
+
+//Minify the CSS using gulp-clean-css
+//Optimize the images using gulp-imagemin
+//Transpile the JS from ES6 to ES5 using gulp-babel
+//Concatenate the JS into a single file (call it main.js) using gulp-concat. These files will need to be concatenated in a specific order so that they are loaded without errors.
+//Minify the JS using gulp-uglify
+//Copy over the HTML.
+//Setup a Gulp watch task that will watch for changes to your CSS, JS, and HTML files and run the necessary Gulp tasks that will generate new files when they change.
+//Setup a gulp task called 'all' to run ALL the gulp tasks.
